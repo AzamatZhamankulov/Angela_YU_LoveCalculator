ecf53870792aecfff890a7f525c36317f93a213e
Azamat Zhamankul <azamat.zhamankulov.kk@gmail.com>
2021-10-16 01:26:28 +0600

    Style inserted

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore >= 70) {
-   document.write(`<h3>Your love score is ${loveScore}% <br>You love each other and be happy</h3>`);
+   document.write(`<h3 style = "color: purple">Your love score is ${loveScore}% <br>You love each other and be happy</h3>`);
 } else if (loveScore < 70 && loveScore > 49) {
-   document.write(`<h3>Your love score is ${loveScore}% <br>You have nice relations you are good friends</h3>`);
+   document.write(`<h3 style = "color: green">Your love score is ${loveScore}% <br>You have nice relations you are good friends</h3>`);
 } else {
-   document.write(`<h3>Your love score is ${loveScore}% <br>RUN AWAY!!!!!</h3>`)
+   document.write(`<h3 style = "color: red">Your love score is ${loveScore}% <br>RUN AWAY!!!!!</h3>`)
 }
 
